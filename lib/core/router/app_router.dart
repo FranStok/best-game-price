6f2acf60872fb86100feaa642d648541e4b99bfd
F-Astoreca <fastoreca@sinergiasoluciones.com.ar>
2024-11-22 09:30:50 -0300

    A ver

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,22 +1,22 @@
 part of '../../main.dart';
 
 final GoRouter _router = GoRouter(
-    initialLocation: AppRoutes.home.path,
+    initialLocation: AppRoutes.login.path,
     errorBuilder: (context, state) {
       return const ErrorPage();
     },
     routes: [
       GoRoute(
         path: AppRoutes.home.path,
-        // redirect: (context, state) {
-        //   if (BlocProvider.of<SessionCubit>(context)
-        //           .state
-        //           .userCredentials ==
-        //       null) {
-        //     return AppRoutes.login.path;
-        //   }
-        //   return null;
-        // },
+        redirect: (context, state) {
+          if (BlocProvider.of<SessionCubit>(context)
+                  .state
+                  .userCredentials ==
+              null) {
+            return AppRoutes.login.path;
+          }
+          return null;
+        },
         builder: (context, state) {
           return const HomePage(); //TODO PONER REDIRECT DE VUELTA
         },
